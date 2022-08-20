79de439cfe39753a39dd0a2e3281a305b06f4018
Daivuk <daivuk@gmail.com>
2022-08-20 16:53:16 -0400

    virtual fn instead of drawlist

--- include/ogui/ogui.h
+++ include/ogui/ogui.h
@@ -8,17 +8,7 @@
 
 namespace ogui
 {
-    enum class eDrawCommand : int32_t
-    {
-        Draw,
-        SetScissor,
-        SetTexture,
-        CreateTexture,
-        UpdateTexture,
-        DestroyTexture,
-        UserDraw
-    };
-
+    // Structures
     struct Vec2
     {
         float x, y;
@@ -34,56 +24,6 @@ namespace ogui
         Vec2 position;
         Vec2 uv;
         uint32_t color;
-    };
-
-    typedef void (*UserDrawFn)(void *pUserData, const uint32_t *pViewport);
-
-    struct DrawCommand
-    {
-        eDrawCommand command;
-        union
-        {
-            struct DrawData
-            {
-                uint32_t vertexStart, vertexCount;
-            } drawData;
-
-            struct ScissorData
-            {
-                uint32_t x, y, width, height;
-            } scissorData;
-
-            struct SetTextureData
-            {
-                uintptr_t textureId;
-            } setTextureData;
-
-            struct CreateTextureData
-            {
-                uintptr_t *pTextureId;
-                uint32_t width, height;
-                uint8_t *pData;
-            } createTextureData;
-
-            struct UpdateTextureData
-            {
-                uintptr_t textureId;
-                uint32_t width, height;
-                uint8_t *pData;
-            } updateTextureData;
-
-            struct DestroyTextureData
-            {
-                uintptr_t textureId;
-            } destroyTextureData;
-
-            struct UserDrawData
-            {
-                UserDrawFn userDrawFn;
-                void *pUserData;
-                uint32_t x, y, width, height;
-            } userDrawData;
-        };
     };
     
     struct Color
@@ -129,65 +69,67 @@ namespace ogui
         float boolControlWidth;
         float textControlWidth;
 
-        Color windowColor;;
-        Color darkColor;;
-        Color panelColor;;
-        Color panelBorderColor;;
-        Color areaColor;;
-        Color areaBorderColor;;
-        Color controlColor;;
-        Color controlBorderColor;;
-        Color activeColor;;
-        Color disabledControlColor;;
-        Color disabledControlBorderColor;;
-        Color separatorColor;;
-        Color textColor;;
-        Color textOverColor;;
-        Color disabledTextColor;;
-        Color toolButtonColor;;
-        Color toolButtonHover;;
-        Color toolButtonDown;;
-        Color inactiveTabColor;;
-        Color dockColor;;
-        Color disabledTint;;
-        Color headerColor;;
+        Color windowColor;
+        Color darkColor;
+        Color panelColor;
+        Color panelBorderColor;
+        Color areaColor;
+        Color areaBorderColor;
+        Color controlColor;
+        Color controlBorderColor;
+        Color activeColor;
+        Color disabledControlColor;
+        Color disabledControlBorderColor;
+        Color separatorColor;
+        Color textColor;
+        Color textOverColor;
+        Color disabledTextColor;
+        Color toolButtonColor;
+        Color toolButtonHover;
+        Color toolButtonDown;
+        Color inactiveTabColor;
+        Color dockColor;
+        Color disabledTint;
+        Color headerColor;
     };
+    
+    typedef void (*UserDrawFn)(void *pUserData, const uint32_t *pViewport);
 
-    class Context final
+    struct DrawCommand;
+
+    class IRenderer
     {
     public:
-        Context();
-        ~Context();
+        // Those happen before beginFrame()
+        virtual uintptr_t createTexture(uint32_t width, uint32_t height, uint8_t *pData) = 0;
+        virtual void updateTexture(uintptr_t textureId, uint32_t width, uint32_t height, uint8_t *pData) = 0;
+        virtual void destroyTexture(uintptr_t textureId) = 0;
 
-        void update(int width, int height); // Update GUI and fire events. Pass in the current application width/height
-        void render(); // Generate draw list
+        // The draw calls
+        virtual void beginFrame() = 0; // Called first
+        virtual void setVertexData(const Vertex *pData, uint32_t count) = 0;
+        virtual void draw(uint32_t startOffset, uint32_t count) = 0;
+        virtual void scissor(uint32_t x, uint32_t y, uint32_t width, uint32_t height) = 0;
+        virtual void bindTexture(uintptr_t textureId) = 0;
+        virtual void userDraw(UserDrawFn userDrawFn, void *pUserData, const uint32_t *viewport) = 0;
+        virtual void endFrame() = 0; // Called last
+    };
 
-        const std::vector<Vertex> &getVertices() const { return m_vertices; }
-        const std::vector<DrawCommand> &getDrawList() const { return m_drawList; }
+    class IContext
+    {
+    public:
+        static IContext *create(IRenderer *pRenderer);
 
-        const Theme &getTheme() const { return m_theme; };
-        void setTheme(const Theme &theme);
+        virtual ~IContext() {}
 
-    private:
-        struct Texture
-        {
-            uint8_t *pData;
-            uint32_t width, height;
-            uintptr_t id;
-        };
+        virtual void update(int width, int height) = 0; // Update GUI and fire events. Pass in the current application width/height
+        virtual void render() = 0; // Generate draw list
 
-        void bindTexture(const Texture &texture);
-        void drawRect(const Rect &rect, const Color &color);
+        virtual const Theme &getTheme() const = 0;
+        virtual void setTheme(const Theme &theme) = 0;
 
-        std::vector<Vertex> m_vertices;
-        std::vector<DrawCommand> m_drawList;
-        std::vector<Texture *> m_textureToCreate;
-        std::vector<Texture *> m_textureToUpdate;
-        std::vector<Texture *> m_textureToDestroy;
-        std::unordered_map<std::string, Texture *> m_textureByFilename;
-        Theme m_theme;
-
-        Texture m_whiteTexture;
+    protected:
+        IContext() {}
     };
 }
 
--- src/ogui.cpp
+++ src/ogui.cpp
@@ -1,8 +1,69 @@
 #include "ogui/ogui.h"
 
+#include <cassert>
+
 namespace ogui
 {
     static uint32_t WHITE = 0xFFFFFFFF;
+
+    enum class eDrawCommand : int32_t
+    {
+        Draw,
+        SetScissor,
+        SetTexture,
+        CreateTexture,
+        UpdateTexture,
+        DestroyTexture,
+        UserDraw
+    };
+
+    struct DrawCommand
+    {
+        eDrawCommand command;
+        union
+        {
+            struct DrawData
+            {
+                uint32_t vertexStart, vertexCount;
+            } drawData;
+
+            struct ScissorData
+            {
+                uint32_t x, y, width, height;
+            } scissorData;
+
+            struct SetTextureData
+            {
+                uintptr_t textureId;
+            } setTextureData;
+
+            struct CreateTextureData
+            {
+                uintptr_t *pTextureId;
+                uint32_t width, height;
+                uint8_t *pData;
+            } createTextureData;
+
+            struct UpdateTextureData
+            {
+                uintptr_t textureId;
+                uint32_t width, height;
+                uint8_t *pData;
+            } updateTextureData;
+
+            struct DestroyTextureData
+            {
+                uintptr_t textureId;
+            } destroyTextureData;
+
+            struct UserDrawData
+            {
+                UserDrawFn userDrawFn;
+                void *pUserData;
+                uint32_t x, y, width, height;
+            } userDrawData;
+        };
+    };
 
     static Color HexToColor(uint32_t hex)
     {
@@ -24,139 +85,204 @@ namespace ogui
         return packed;
     }
 
-    Context::Context()
+    class Context final : public IContext
     {
-        // Setup default theme
-        m_theme.panelMargin = 8.0f;
-        m_theme.panelPadding = 5.0f;
-        m_theme.controlHeight = 26.0f;
-        m_theme.headerHeight = 20.0f;
-        m_theme.listItemHeight = 20.0f;
-        m_theme.borderSize = 1.0f;
-        m_theme.tabSpacing = 0.0f;
-        m_theme.tabPadding = 8.0f;
-        m_theme.toolButtonSize = 16.0f;
-        m_theme.toolBarHeight = 20.0f;
-        m_theme.treeIndent = 16.0f;
-        m_theme.minHSize = 200.0f;
-        m_theme.minVSize = 100.0f;
-        m_theme.controlMargin = 8.0f;
-        m_theme.controlPadding = 4.0f;
-        m_theme.controlSpacing = 2.0f;
-        m_theme.numericControlWidth = 40.0f;
-        m_theme.boolControlWidth = 40.0f;
-        m_theme.textControlWidth = 150.0f;
+    public:
+        struct Texture
+        {
+            uint8_t *pData;
+            uint32_t width, height;
+            uintptr_t id;
+        };
 
-        m_theme.windowColor = HexToColor(0x202531);
-        m_theme.darkColor = HexToColor(0x101218);
-        m_theme.panelColor = HexToColor(0x333b4f);
-        m_theme.panelBorderColor = HexToColor(0x191d27);
-        m_theme.areaColor = HexToColor(0x262c3b);
-        m_theme.areaBorderColor = HexToColor(0x191d27);
-        m_theme.controlColor = HexToColor(0x262c3b);
-        m_theme.controlBorderColor = HexToColor(0x202531);
-        m_theme.activeColor = HexToColor(0x699ce8);
-        m_theme.disabledControlColor = HexToColor(0x2e3547);
-        m_theme.disabledControlBorderColor = HexToColor(0x2e3547);
-        m_theme.separatorColor = HexToColor(0x333b4f);
-        m_theme.textColor = HexToColor(0xe0e0e0);
-        m_theme.textOverColor = HexToColor(0xffffff);
-        m_theme.disabledTextColor = HexToColor(0x777a82);
-        m_theme.toolButtonColor = HexToColor(0xe0e0e0);
-        m_theme.toolButtonHover = HexToColor(0xffffff);
-        m_theme.toolButtonDown = HexToColor(0x699ce8);
-        m_theme.inactiveTabColor = HexToColor(0x262c3b);
-        m_theme.dockColor = { m_theme.activeColor.r * 0.5f, m_theme.activeColor.g * 0.5f, m_theme.activeColor.b * 0.5f, m_theme.activeColor.a * 0.5f };
-        m_theme.disabledTint = { 0.5f, 0.5f, 0.5f, 1.0f };
-        m_theme.headerColor = HexToColor(404553);
+        IRenderer *m_pRenderer = nullptr;
 
-        m_whiteTexture.width = 1;
-        m_whiteTexture.height = 1;
-        m_whiteTexture.pData = (uint8_t *)&WHITE;
-        m_textureToCreate.push_back(&m_whiteTexture);
-    }
+        std::vector<Vertex> m_vertices;
+        std::vector<DrawCommand> m_drawList;
+        std::vector<Texture *> m_textureToCreate;
+        std::vector<Texture *> m_textureToUpdate;
+        std::vector<Texture *> m_textureToDestroy;
+        std::unordered_map<std::string, Texture *> m_textureByFilename;
+        Theme m_theme;
 
-    Context::~Context()
-    {
-    }
+        Texture m_whiteTexture;
 
-    void Context::setTheme(const Theme &theme)
-    {
-    }
+        Context(IRenderer *pRenderer)
+            : m_pRenderer(pRenderer)
+        {
+            // Setup default theme
+            m_theme.panelMargin = 8.0f;
+            m_theme.panelPadding = 5.0f;
+            m_theme.controlHeight = 26.0f;
+            m_theme.headerHeight = 20.0f;
+            m_theme.listItemHeight = 20.0f;
+            m_theme.borderSize = 1.0f;
+            m_theme.tabSpacing = 0.0f;
+            m_theme.tabPadding = 8.0f;
+            m_theme.toolButtonSize = 16.0f;
+            m_theme.toolBarHeight = 20.0f;
+            m_theme.treeIndent = 16.0f;
+            m_theme.minHSize = 200.0f;
+            m_theme.minVSize = 100.0f;
+            m_theme.controlMargin = 8.0f;
+            m_theme.controlPadding = 4.0f;
+            m_theme.controlSpacing = 2.0f;
+            m_theme.numericControlWidth = 40.0f;
+            m_theme.boolControlWidth = 40.0f;
+            m_theme.textControlWidth = 150.0f;
 
-    void Context::update(int width, int height)
-    {
-    }
+            m_theme.windowColor = HexToColor(0x202531);
+            m_theme.darkColor = HexToColor(0x101218);
+            m_theme.panelColor = HexToColor(0x333b4f);
+            m_theme.panelBorderColor = HexToColor(0x191d27);
+            m_theme.areaColor = HexToColor(0x262c3b);
+            m_theme.areaBorderColor = HexToColor(0x191d27);
+            m_theme.controlColor = HexToColor(0x262c3b);
+            m_theme.controlBorderColor = HexToColor(0x202531);
+            m_theme.activeColor = HexToColor(0x699ce8);
+            m_theme.disabledControlColor = HexToColor(0x2e3547);
+            m_theme.disabledControlBorderColor = HexToColor(0x2e3547);
+            m_theme.separatorColor = HexToColor(0x333b4f);
+            m_theme.textColor = HexToColor(0xe0e0e0);
+            m_theme.textOverColor = HexToColor(0xffffff);
+            m_theme.disabledTextColor = HexToColor(0x777a82);
+            m_theme.toolButtonColor = HexToColor(0xe0e0e0);
+            m_theme.toolButtonHover = HexToColor(0xffffff);
+            m_theme.toolButtonDown = HexToColor(0x699ce8);
+            m_theme.inactiveTabColor = HexToColor(0x262c3b);
+            m_theme.dockColor = { m_theme.activeColor.r * 0.5f, m_theme.activeColor.g * 0.5f, m_theme.activeColor.b * 0.5f, m_theme.activeColor.a * 0.5f };
+            m_theme.disabledTint = { 0.5f, 0.5f, 0.5f, 1.0f };
+            m_theme.headerColor = HexToColor(404553);
 
-    void Context::render()
-    {
-        m_vertices.clear();
-        m_drawList.clear();
+            m_whiteTexture.width = 1;
+            m_whiteTexture.height = 1;
+            m_whiteTexture.pData = (uint8_t *)&WHITE;
+            m_textureToCreate.push_back(&m_whiteTexture);
+        }
 
-        // Create textures
-        for (auto &textureToCreate : m_textureToCreate)
+        ~Context() {}
+
+        void update(int width, int height) override
+        {
+        }
+
+        void render() override
+        {
+            m_vertices.clear();
+            m_drawList.clear();
+
+            // Create textures
+            for (auto &textureToCreate : m_textureToCreate)
+            {
+                DrawCommand cmd;
+                cmd.command = eDrawCommand::CreateTexture;
+                cmd.createTextureData.pTextureId = &textureToCreate->id;
+                cmd.createTextureData.width = textureToCreate->width;
+                cmd.createTextureData.height = textureToCreate->height;
+                cmd.createTextureData.pData = textureToCreate->pData;
+                m_drawList.push_back(cmd);
+            }
+            m_textureToCreate.clear();
+
+            // Update textures
+            for (auto &textureToUpdate : m_textureToUpdate)
+            {
+                DrawCommand cmd;
+                cmd.command = eDrawCommand::UpdateTexture;
+                cmd.updateTextureData.textureId = textureToUpdate->id;
+                cmd.updateTextureData.width = textureToUpdate->width;
+                cmd.updateTextureData.height = textureToUpdate->height;
+                cmd.updateTextureData.pData = textureToUpdate->pData;
+                m_drawList.push_back(cmd);
+            }
+            m_textureToUpdate.clear();
+
+            // Destroy textures
+            for (auto &textureToDestroy : m_textureToDestroy)
+            {
+                DrawCommand cmd;
+                cmd.command = eDrawCommand::UpdateTexture;
+                cmd.destroyTextureData.textureId = textureToDestroy->id;
+                m_drawList.push_back(cmd);
+            }
+            m_textureToDestroy.clear();
+
+            // Generate drawlist
+            bindTexture(m_whiteTexture);
+            drawRect({ 50, 100, 150, 25 }, { 1, 0, 0, 1 });
+
+            // Call into the renderer for the actual render
+            m_pRenderer->beginFrame();
+            m_pRenderer->setVertexData(m_vertices.data(), (uint32_t)m_vertices.size());
+
+            for (const auto &cmd : m_drawList)
+            {
+                switch (cmd.command)
+                {
+                    case ogui::eDrawCommand::Draw:
+                        m_pRenderer->draw(cmd.drawData.vertexStart, cmd.drawData.vertexCount);
+                        break;
+                    case ogui::eDrawCommand::SetScissor:
+                        m_pRenderer->scissor(cmd.scissorData.x, cmd.scissorData.y, cmd.scissorData.width, cmd.scissorData.height);
+                        break;
+                    case ogui::eDrawCommand::SetTexture:
+                        m_pRenderer->bindTexture(cmd.setTextureData.textureId);
+                        break;
+                    case ogui::eDrawCommand::CreateTexture:
+                        *cmd.createTextureData.pTextureId = m_pRenderer->createTexture(cmd.createTextureData.width, cmd.createTextureData.height, cmd.createTextureData.pData);
+                        break;
+                    case ogui::eDrawCommand::UpdateTexture:
+                        m_pRenderer->updateTexture(cmd.updateTextureData.textureId, cmd.updateTextureData.width, cmd.updateTextureData.height, cmd.updateTextureData.pData);
+                        break;
+                    case ogui::eDrawCommand::DestroyTexture:
+                        m_pRenderer->destroyTexture(cmd.destroyTextureData.textureId);
+                        break;
+                    case ogui::eDrawCommand::UserDraw:
+                        m_pRenderer->userDraw(cmd.userDrawData.userDrawFn, cmd.userDrawData.pUserData, &cmd.userDrawData.x);
+                        break;
+                };
+            }
+
+            m_pRenderer->endFrame();
+        }
+
+        const Theme &getTheme() const override { return m_theme; };
+
+        void setTheme(const Theme &theme) override
+        {
+        }
+
+        void bindTexture(const Texture &texture)
         {
             DrawCommand cmd;
-            cmd.command = eDrawCommand::CreateTexture;
-            cmd.createTextureData.pTextureId = &textureToCreate->id;
-            cmd.createTextureData.width = textureToCreate->width;
-            cmd.createTextureData.height = textureToCreate->height;
-            cmd.createTextureData.pData = textureToCreate->pData;
+            cmd.command = eDrawCommand::SetTexture;
+            cmd.setTextureData.textureId = m_whiteTexture.id;
             m_drawList.push_back(cmd);
         }
-        m_textureToCreate.clear();
 
-        // Update textures
-        for (auto &textureToUpdate : m_textureToUpdate)
+        void drawRect(const Rect &rect, const Color &color)
         {
+            auto color32 = ColorToHex(color);
+
+            m_vertices.push_back(Vertex{ { rect.x, rect.y }, { 0, 0 }, color32 });
+            m_vertices.push_back(Vertex{ { rect.x, rect.y + rect.h }, { 0, 0 }, color32 });
+            m_vertices.push_back(Vertex{ { rect.x + rect.w, rect.y + rect.h }, { 0, 0 }, color32 });
+            m_vertices.push_back(Vertex{ { rect.x + rect.w, rect.y + rect.h }, { 0, 0 }, color32 });
+            m_vertices.push_back(Vertex{ { rect.x + rect.w, rect.y }, { 0, 0 }, color32 });
+            m_vertices.push_back(Vertex{ { rect.x, rect.y }, { 0, 0 }, color32 });
+
             DrawCommand cmd;
-            cmd.command = eDrawCommand::UpdateTexture;
-            cmd.updateTextureData.textureId = textureToUpdate->id;
-            cmd.updateTextureData.width = textureToUpdate->width;
-            cmd.updateTextureData.height = textureToUpdate->height;
-            cmd.updateTextureData.pData = textureToUpdate->pData;
+            cmd.command = eDrawCommand::Draw;
+            cmd.drawData.vertexStart = 0;
+            cmd.drawData.vertexCount = 6;
             m_drawList.push_back(cmd);
         }
-        m_textureToUpdate.clear();
+    };
 
-        // Destroy textures
-        for (auto &textureToDestroy : m_textureToDestroy)
-        {
-            DrawCommand cmd;
-            cmd.command = eDrawCommand::UpdateTexture;
-            cmd.destroyTextureData.textureId = textureToDestroy->id;
-            m_drawList.push_back(cmd);
-        }
-        m_textureToDestroy.clear();
-
-        // Draw a simple quad to test
-        bindTexture(m_whiteTexture);
-        drawRect({ 50, 100, 150, 25 }, { 1, 0, 0, 1 });
-    }
-
-    void Context::bindTexture(const Texture &texture)
+    IContext *IContext::create(IRenderer *pRenderer)
     {
-        DrawCommand cmd;
-        cmd.command = eDrawCommand::SetTexture;
-        cmd.setTextureData.textureId = m_whiteTexture.id;
-        m_drawList.push_back(cmd);
-    }
-
-    void Context::drawRect(const Rect &rect, const Color &color)
-    {
-        auto color32 = ColorToHex(color);
-
-        m_vertices.push_back(Vertex{ { rect.x, rect.y }, { 0, 0 }, color32 });
-        m_vertices.push_back(Vertex{ { rect.x, rect.y + rect.h }, { 0, 0 }, color32 });
-        m_vertices.push_back(Vertex{ { rect.x + rect.w, rect.y + rect.h }, { 0, 0 }, color32 });
-        m_vertices.push_back(Vertex{ { rect.x + rect.w, rect.y + rect.h }, { 0, 0 }, color32 });
-        m_vertices.push_back(Vertex{ { rect.x + rect.w, rect.y }, { 0, 0 }, color32 });
-        m_vertices.push_back(Vertex{ { rect.x, rect.y }, { 0, 0 }, color32 });
-
-        DrawCommand cmd;
-        cmd.command = eDrawCommand::Draw;
-        cmd.drawData.vertexStart = 0;
-        cmd.drawData.vertexCount = 6;
-        m_drawList.push_back(cmd);
+        assert(pRenderer && "Must have valid renderer.");
+        if (!pRenderer) return nullptr;
+        return new Context(pRenderer);
     }
 }
